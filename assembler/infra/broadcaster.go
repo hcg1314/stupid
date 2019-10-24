2befb666d1dc0c710d6f134478edeb9698b21b32
hucg <hucg>
2019-10-24 18:39:57 +0800

    fix bug

---
+++ assembler/infra/broadcaster.go
@@ -0,0 +1,67 @@
+package infra
+
+import (
+	"fmt"
+	"github.com/hcg1314/stupid/assembler/basic"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/orderer"
+	"io"
+)
+
+type broadcaster struct {
+	c    orderer.AtomicBroadcast_BroadcastClient
+	envs chan *Elements
+}
+
+func CreateBroadcaster(node basic.Node, crypto *basic.Crypto) *broadcaster {
+	client, err := CreateBroadcastClient(node, crypto.TLSCACerts)
+	if err != nil {
+		panic(err)
+	}
+
+	return &broadcaster{
+		c:    client,
+		envs: make(chan *Elements, 1000),
+	}
+}
+
+func (b *broadcaster) Handle(e *Elements) error {
+	b.envs <- e
+	return nil
+}
+
+func (b *broadcaster) Start() {
+	go b.startDraining()
+	for {
+		select {
+		case e,ok := <-b.envs:
+			if !ok {
+				return
+			}
+			err := b.c.Send(e.Envelope)
+			if err != nil {
+				fmt.Printf("Failed to broadcast env: %s\n", err)
+			}
+		}
+	}
+}
+
+func (b *broadcaster) startDraining() {
+	for {
+		res, err := b.c.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+
+			fmt.Printf("Recv broadcast err: %s, status: %+v\n", err, res)
+			panic("bcast recv err")
+		}
+
+		if res.Status != common.Status_SUCCESS {
+			fmt.Printf("Recv errouneous status: %s\n", res.Status)
+			panic("bcast recv err")
+		}
+
+	}
+}
---
+++ assembler/infra/dispatcher.go
@@ -0,0 +1,100 @@
+package infra
+
+import (
+	"github.com/hcg1314/stupid/assembler/basic"
+	"github.com/hyperledger/fabric/protos/common"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type Elements struct {
+	Proposal   *peer.Proposal
+	SignedProp *peer.SignedProposal
+	Response   *peer.ProposalResponse
+	Envelope   *common.Envelope
+}
+
+type Handler interface {
+	Handle(e *Elements) error
+}
+
+type Dispatcher struct {
+	input        chan *Elements
+	output       chan *Elements
+	handlers     []Handler
+	handlerCount int
+}
+
+func CreateProposalDispatcher(conn, client int, nodes []basic.Node, crypto *basic.Crypto) *Dispatcher {
+
+	count := conn * len(nodes) // peer节点数*每个节点的tcp连接数
+	dispatch := &Dispatcher{
+		input:        make(chan *Elements, 1000),
+		output:       make(chan *Elements, 1000),
+		handlerCount: count,
+		handlers:     make([]Handler, count),
+	}
+
+	index := 0
+	for _, node := range nodes {
+		for i := 0; i < conn; i++ {
+			proposer := CreateProposer(node, crypto, client)
+			proposer.Start(dispatch.output)
+			dispatch.handlers[index] = proposer
+			index += 1
+		}
+	}
+
+	return dispatch
+}
+
+func CreateBroadcastDispatcher(conn int, node basic.Node, crypto *basic.Crypto) *Dispatcher {
+	dispatch := &Dispatcher{
+		input:        make(chan *Elements, 1000),
+		output:       nil,
+		handlerCount: conn,
+		handlers:     make([]Handler, conn),
+	}
+
+	for i := 0; i < conn; i++ {
+		broadcaster := CreateBroadcaster(node, crypto)
+		go broadcaster.Start()
+		dispatch.handlers[i] = broadcaster
+	}
+
+	return dispatch
+}
+
+func (d *Dispatcher) Start() {
+	index := 0
+	for {
+		select {
+		case msg, ok := <-d.input:
+			if !ok {
+				return
+			}
+			if index >= d.handlerCount {
+				index = 0
+			}
+
+			_ = d.handlers[index].Handle(msg)
+			index += 1
+		}
+	}
+}
+
+func (d *Dispatcher) Send(e *Elements) {
+	d.input <- e
+}
+
+func (d *Dispatcher) GetOutput() chan *Elements {
+	return d.output
+}
+
+func (d *Dispatcher) GetStatisticInfo() string {
+	info := "Endorser Statistic:\n" +
+		"\t     Total(     Speed)   Success(     Speed)      Fail(     Speed)\n"
+	/*for _, p := range d.handlers {
+		info += p.GetStatisticInfo()
+	}*/
+	return info
+}
